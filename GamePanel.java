b308c2137db2857f009caa999744a7ec579a671b
ilya.kost <ilyalife33@gmail.com>
2025-06-25 09:26:26 +0200

    Key Input + FPS(Game Loop)

--- GamePanel.java
+++ GamePanel.java
@@ -1,4 +1,5 @@
-
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JPanel;
@@ -12,8 +13,16 @@ public class GamePanel extends JPanel implements Runnable
     final int maxScreenRow = 12; // blöcke horizontal
     final int screenWidth = tileSize * maxScreenCol; // 1104pixel länge vom Screen
     final int screenHeight = tileSize * maxScreenRow; // 576pixel Höhe vom Screen
+    //FPS
+    int FPS = 60;
     //erstellt ein Thread und gleichzeitig startet die Uhr/Zeit. Programm wird laufen bis man es stoppt. Hilft das Prozzes zu wiederholen(FPS)    
     Thread gameThread;
+    KeyHandler keyH = new KeyHandler();
+
+    //Spieler position am Anfang
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4; 
 
     public GamePanel()
     {
@@ -22,31 +31,63 @@ public class GamePanel extends JPanel implements Runnable
         this.setBackground(Color.pink);
         // Drawings werden in offscreen painting buffer gemacht
         this.setDoubleBuffered(true); 
+        this.addKeyListener(keyH);
+        this.setFocusable(true); // this GamePanel ist focused um key Input zu bekommen
     }
-    
+
     public void startGameThread()
     {
         gameThread = new Thread(this);
         gameThread.start();
     }
-    
+
     @Override 
     public void run()
     {
-        while(gameThread != null) //wiederholt das Prozess
+
+        double drawInterval = 1000000000/FPS; // 0.01666 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
+        while(gameThread != null){ //wiederholt das Prozess
             // 1) Update position
             update();
             // 2) Draw the screen 
             repaint();
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000; // convert to milliseconds 
+                if(remainingTime < 0){
+                    remainingTime = 0;
+                }
+                Thread.sleep((long) remainingTime);
+                
+                nextDrawTime = nextDrawTime + drawInterval;
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
     }
 
-}
-public void update() {
-}
-public void paintComponent(Graphics g) {
-    super.paintComponent(g); // super = subclass von JPanel
-Graphics2D g2  = (Graphics2D)g; //Graphics2D erweitert class Graphics für mehr kontrolle über color managment
-g2.setColor(Color.white);
-g2.fillRect(100, 100, tileSize, tileSize); // (x, y, weidth, height)
-g2.dispose();
+    public void update() {
+        if(keyH.upPressed == true){
+            playerY = playerY - playerSpeed;
+        }
+        else if(keyH.downPressed == true ){
+            playerY = playerY + playerSpeed;
+        }
+        else if(keyH.leftPressed == true ){
+            playerX = playerX - playerSpeed;
+        }
+        else if(keyH.rightPressed == true ){
+            playerX = playerX + playerSpeed;
+        }
     }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g); // super = subclass von JPanel
+        Graphics2D g2  = (Graphics2D)g; //Graphics2D erweitert class Graphics für mehr kontrolle über color managment
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, tileSize, tileSize); // (x, y, weidth, height)
+        g2.dispose();
+    }
+}
